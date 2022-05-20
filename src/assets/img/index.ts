6442d19128edba324b01fd9045dc63cd856edc12
IntegralTeam_RK <repo@sfxdx.ru>
2022-05-20 08:52:30 +0300

    Fix bug file name

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,1 +1,8 @@
 export { default as LogoImg } from './logo.svg';
+export { default as BridgeLogo } from './bridgeLogo.svg';
+export { default as PlugIcon } from './plugIcon.svg';
+export { default as MetamaskIcon } from './metamaskIcon.svg';
+export { default as ChangeIcon } from './changeIcon.svg';
+export { default as RedEllipse } from './redEllipse.svg';
+export { default as BlueEllipse } from './blueEllipse.svg';
+export { default as GreenEllipse } from './greenEllipse.svg';
