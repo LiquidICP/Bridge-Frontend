40dda3bc35009f0c7be45bedfd152497117aa95d
IntegralTeam_RK <repo@sfxdx.ru>
2022-05-23 10:37:03 +0300

    clear main

--- src/containers/Main/index.tsx
+++ src/containers/Main/index.tsx
@@ -1,14 +1,10 @@
 import React, { FC } from 'react';
-import { Button } from 'components';
 import styles from './styles.module.css';
 import { Tabs } from './Tabs';
 
 const Main: FC = () => (
   <main className={styles.main__container}>
     <Tabs />
-    <Button theme="icon" onClick={() => {}}>
-      Ba
-    </Button>
   </main>
 );
 
