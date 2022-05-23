43339d454514ee3996d878d0723e95c6eabc7e2b
IntegralTeam_DI <repo@sfxdx.ru>
2022-05-23 19:55:17 +0300

    congrats modal

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,5 @@ export { default as createReducer } from './createReducer';
 export { throwApiError } from './throwApiError';
 export { sagaExceptionHandler } from './sagaExceptionHandler';
 export { transformBackendErrorToString } from './transformBackendErrorToString';
+export { getShortAddress } from './getShortAddress';
+export { rootBlur } from './rootBlur';
