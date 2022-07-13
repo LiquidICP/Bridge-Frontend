22207e23a976104fd0e60b153b66906cd441ab4b
IntegralTeam_RK <repo@sfxdx.ru>
2022-07-13 15:05:02 +0300

    Intermediate commit

--- src/store/metamask/sagas/balanceSagas.ts
+++ src/store/metamask/sagas/balanceSagas.ts
@@ -17,11 +17,13 @@ import { MetamaskState } from '../types';
 export const getTokensBalance = async (
   addressWallet: string,
 ) => {
+  // eslint-disable-next-line no-debugger
+  debugger;
   const contract = await getContract();
-  console.log('Contract', contract);
-  console.log(await contract.balanceOf(addressWallet));
+  // console.log('Contract', contract);
+  // console.log(await contract.balanceOf(addressWallet));
   const balance: ethers.BigNumber = await contract.balanceOf(addressWallet);
-  console.log('Balance:', balance);
+  // console.log('Balance:', balance);
   return balance.toString();
 };
 
@@ -29,20 +31,20 @@ export function* getTokensBalanceSaga() {
   try {
     yield put(metamaskSetState({ isTokensBalanceLoading: true }));
 
-    console.log('get balance step 1');
+    // console.log('get balance step 1');
     const address: MetamaskState['address'] = yield select(metamaskSelectors.getProp('address'));
-    console.log('get balance step 2');
+    // console.log('get balance step 2');
     if (address) {
-      console.log('address', address);
-      console.log('get balance step 3. Is Address');
+      // console.log('address', address);
+      // console.log('get balance step 3. Is Address');
       const balanceWei: string = yield getTokensBalance(address);
-      console.log('get balance step 4');
+      // console.log('get balance step 4');
       const tokensBalance: string = yield ethers.utils.formatUnits(balanceWei, 8);
-      console.log('get balance step 5 token balance');
+      // console.log('get balance step 5 token balance');
       yield put(metamaskSetState({ tokensBalance: Number(tokensBalance) }));
     }
   } catch (err) {
-    console.log('Error getBalanceMetamask', err);
+    // console.log('Error getBalanceMetamask', err);
     // sagaExceptionHandler('Unable to get balance'); ****** disable *********
   } finally {
     yield put(metamaskSetState({ isTokensBalanceLoading: false }));
